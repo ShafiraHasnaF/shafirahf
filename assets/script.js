e975802445153c9ddcad30666a3e0eb3fc73cf4a
Shafira Hasna Fauzi <shafirahasna32@gmail.com>
2026-02-15 17:19:41 +0700

    easter egg usinh matter.js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const text = "C:/Users/Visitor>"; 
+const text = "C:/Users/Visitor>";
 const textSpeed = 200;
 const textDelay = 6000;
 let index = 0;
@@ -29,4 +29,95 @@ navItems.forEach(item => {
         })
         document.getElementById(target).classList.add("active");
     });
+});
+const { Engine, World, Bodies, Events } = Matter;
+
+const btn = document.getElementById("unpin");
+const wrapper = document.querySelector(".tools-wrapper");
+const grid = document.querySelector(".tools-grid");
+const physicsContainer = document.querySelector(".container-gravitasi");
+const items = document.querySelectorAll(".tool-item");
+
+let engine;
+let activated = false;
+
+btn.addEventListener("click", () => {
+
+    if (activated) return;
+    activated = true;
+
+    const rect = wrapper.getBoundingClientRect();
+
+    engine = Engine.create();
+    const world = engine.world;
+
+    Engine.run(engine);
+
+    // === TEMBOK ===
+    const ground = Bodies.rectangle(
+        rect.width / 2,
+        rect.height + 20,
+        rect.width,
+        40,
+        { isStatic: true }
+    );
+
+    const leftWall = Bodies.rectangle(
+        -20,
+        rect.height / 2,
+        40,
+        rect.height,
+        { isStatic: true }
+    );
+
+    const rightWall = Bodies.rectangle(
+        rect.width + 20,
+        rect.height / 2,
+        40,
+        rect.height,
+        { isStatic: true }
+    );
+
+    World.add(world, [ground, leftWall, rightWall]);
+
+    // sembunyikan flex
+    grid.style.visibility = "hidden";
+
+    items.forEach(item => {
+
+        const itemRect = item.getBoundingClientRect();
+
+        const startX = itemRect.left - rect.left + itemRect.width / 2;
+        const startY = itemRect.top - rect.top + itemRect.height / 2;
+
+        const body = Bodies.rectangle(
+            startX,
+            startY,
+            itemRect.width,
+            itemRect.height,
+            {
+                restitution: 0.4,
+                friction: 0.5
+            }
+        );
+
+        World.add(world, body);
+
+        const clone = item.cloneNode(true);
+        clone.style.position = "absolute";
+        clone.style.width = itemRect.width + "px";
+        clone.style.height = itemRect.height + "px";
+        clone.style.left = (startX - itemRect.width / 2) + "px";
+        clone.style.top = (startY - itemRect.height / 2) + "px";
+        clone.style.margin = 0;
+
+        physicsContainer.appendChild(clone);
+
+        Events.on(engine, "afterUpdate", () => {
+            clone.style.left = body.position.x - itemRect.width / 2 + "px";
+            clone.style.top = body.position.y - itemRect.height / 2 + "px";
+            clone.style.transform = `rotate(${body.angle}rad)`;
+        });
+    });
+
 });
